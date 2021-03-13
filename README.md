269cd4310ae2984d2ee03b2d02535b8f701ba326
ww545 <2998520998@qq.com>
2021-03-13 19:23:18 +0800

    add vue

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# vue-set-star-rating
-vue-star-rating
+# 安装使用 #
+
+```
+npm i vue-stars-rating -S  OR yarn vue-stars-rating
+ 
+```
+
+# 说明 #
+
+>改自 原创 JonathanDn 的开源组件 
+
+
+# 功能 #
+
+>1、点击评分 Click score
+>2、静态展示 Static display
+
+
+
+| parmas |value | remarks |
+|--|--|--|
+|clickChange|Boolean| 默认 false, 是否可以点击；false为不可以点击 |
+|config|Object|具体查看下面示例讲解|
+|stroke|String|描边颜色，边框|
+|itemclick|Fn|点击事件回调名称 @itemclick="itemclick"  该函数有三个参数（num=评分，i=star下标，event=原型）|
+
+
+
+# 完整用法 #
+
+```
+ <template>  
+    <div>
+      <stars-rating clickChange  stroke="#ECB871" v-bind="config" @itemclick="itemclick"></stars-rating>  
+    </div>
+  </template>
+```
+
+```
+ data() {
+      return {
+        config: {
+          rating: 4.9,//设置评分
+          isIndicatorActive: false,//是否显示测试数据到页面上
+          starStyle: {
+            fullStarColor: "#ECB871",//填充颜色
+            emptyStarColor: "rgba(0,0,0,0)",//背景填充颜色
+            starWidth: 100,//宽度
+            starHeight: 100//高度
+          }
+        }
+      };
+ },
+
+```
+
+```
+    methods: {
+      itemclick(num) {       
+        this.$set(this.config,"rating", num); 
+      }
+    },
+
+
+```
---
+++ star-set-rating.vue
@@ -0,0 +1,193 @@
+<template>
+  <div>
+    <div class="star-rating">
+      <div v-for="(star, index) in stars" :key="index" class="star-container"
+        @click="clickChange && nowStar($event,index)">
+        <svg class="star-svg" :style="[
+            { fill: `url(#gradient${star.raw})` },
+            { width: styleStarWidth },
+            { height: styleStarHeight },
+            { stroke: stroke },
+            {strokeWidth:`1px`},
+          ]">
+          <polygon :points="getStarPoints" style="fill-rule: evenodd" />
+          <defs>
+            <!--
+              id has to be unique to each star fullness(dynamic offset) - it indicates fullness above
+            -->
+            <linearGradient :id="`gradient${star.raw}`">
+              <stop id="stop1" :offset="star.percent" stop-opacity="1" :stop-color="getFullFillColor(star)"></stop>
+              <stop id="stop2" :offset="star.percent" stop-opacity="0" :stop-color="getFullFillColor(star)"></stop>
+              <stop id="stop3" :offset="star.percent" stop-opacity="1" :stop-color="styleEmptyStarColor"></stop>
+              <stop id="stop4" offset="100%" stop-opacity="1" :stop-color="styleEmptyStarColor"></stop>
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+      <div v-if="isIndicatorActive" class="indicator">{{ rating }}</div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "stars-rating",
+    components: {},
+    props: {
+      rating: {
+        type: Number,
+        default: 4.3,
+      },
+      stroke:{
+        type:String,        
+      },
+      starStyle: {
+        type: Object,
+      },
+      isIndicatorActive: {
+        type: Boolean,
+        default: true,
+      },
+      clickChange: {
+        type: Boolean,
+        default: false,
+      }
+    },
+    data: function () {
+      return {
+        endnum: 0,
+        stars: [],
+        emptyStar: 0,
+        fullStar: 1,
+        totalStars: 5,
+        // Binded Nested Props registered as data/computed and not props
+        styleStarWidth: 100,
+        styleStarHeight: 100,
+        styleEmptyStarColor: "gray",
+        styleFullStarColor: "goldenrod",
+      };
+    },
+    watch: {
+      rating: function () {
+        this.setNestedConfigStyles(this.starStyle);
+        this.stars = [];
+        this.initStars();
+        this.setStars();
+      }
+    },
+    directives: {},
+    computed: {
+      getStarPoints: function () {
+        let centerX = this.styleStarWidth / 2;
+        let centerY = this.styleStarWidth / 2;
+
+        let innerCircleArms = 5; // a 5 arms star
+
+        let innerRadius = this.styleStarWidth / innerCircleArms;
+        let innerOuterRadiusRatio = 2.5; // Unique value - determines fatness/sharpness of star
+        let outerRadius = innerRadius * innerOuterRadiusRatio;
+
+        return this.calcStarPoints(
+          centerX,
+          centerY,
+          innerCircleArms,
+          innerRadius,
+          outerRadius
+        );
+      },
+    },
+    methods: {
+      nowStar(e, i) {
+        let num = Math.round(e.layerX/10);
+        if(num>=9.5){
+          this.$emit('itemclick', i+1, i,e)
+        }else{
+          this.$emit('itemclick', Number(i+ "." +num), i,e)
+        }
+        
+      },
+      calcStarPoints(
+        centerX,
+        centerY,
+        innerCircleArms,
+        innerRadius,
+        outerRadius
+      ) {
+        let angle = Math.PI / innerCircleArms;
+        let angleOffsetToCenterStar = 60;
+
+        let totalArms = innerCircleArms * 2;
+        let points = "";
+        for (let i = 0; i < totalArms; i++) {
+          let isEvenIndex = i % 2 == 0;
+          let r = isEvenIndex ? outerRadius : innerRadius;
+          let currX = centerX + Math.cos(i * angle + angleOffsetToCenterStar) * r;
+          let currY = centerY + Math.sin(i * angle + angleOffsetToCenterStar) * r;
+          points += currX + "," + currY + " ";
+        }
+        return points;
+      },
+      initStars() {
+
+        for (let i = 0; i < this.totalStars; i++) {
+          this.stars.push({
+            raw: this.emptyStar,
+            percent: this.emptyStar + "%",
+          });
+        }
+      },
+      setStars() {
+        let fullStarsCounter = Math.floor(this.rating);
+        for (let i = 0; i < this.stars.length; i++) {
+          if (fullStarsCounter !== 0) {
+            this.stars[i].raw = this.fullStar;
+            this.stars[i].percent = this.calcStarFullness(this.stars[i]);
+            fullStarsCounter--;
+          } else {
+            let surplus = Math.round((this.rating % 1) * 10) / 10; // Support just one decimal
+            let roundedOneDecimalPoint = Math.round(surplus * 10) / 10;
+            this.stars[i].raw = roundedOneDecimalPoint;
+            return (this.stars[i].percent = this.calcStarFullness(this.stars[i]));
+          }
+        }
+      },
+      getFullFillColor(starData) {
+        return starData.raw !== this.emptyStar ?
+          this.styleFullStarColor :
+          this.styleEmptyStarColor;
+      },
+      calcStarFullness(starData) {
+        let starFullnessPercent = starData.raw * 100 + "%";
+        return starFullnessPercent;
+      },
+      setNestedConfigStyles(objToFlatten) {
+        if (typeof objToFlatten === "object") {
+          for (let i in objToFlatten) {
+            let newKey =
+              "style" + i.charAt(0).toUpperCase() + i.substring(1, i.length);
+            this[newKey] = objToFlatten[i];
+          }
+        }
+      },
+    },
+    created() {
+      this.setNestedConfigStyles(this.starStyle);
+      this.initStars();
+      this.setStars();
+    },
+  };
+</script>
+
+<style scoped >
+.star-rating {
+    display: flex;
+    align-items: center;
+}
+.star-rating .star-container {
+      display: flex;
+}
+.star-rating .star-container:not(:last-child) {
+      margin-right: 5px;
+}
+</style>
